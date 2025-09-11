17e8ed40911903527b412517bdd1d4ab40f3b512
George <gosha3484@gmail.com>
2025-09-11 10:06:35 +0300

    added back start

---
+++ src/test/java/com/example/memorymaster/MemoryMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memorymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
