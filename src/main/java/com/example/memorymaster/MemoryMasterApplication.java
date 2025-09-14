0f580df925c1ea328657726301f5388d5be71e6d
George <gosha3484@gmail.com>
2025-09-14 15:04:29 +0300

    refactored . added user services . added endpoint .

--- src/main/java/com/example/memorymaster/MemoryMasterApplication.java
+++ src/main/java/com/example/memorymaster/MemoryMasterApplication.java
@@ -1,33 +1,16 @@
 package com.example.memorymaster;
 
-import com.example.memorymaster.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.context.annotation.Bean;
-import com.example.memorymaster.entity.User;
 
 @SpringBootApplication
 @RestController
 public class MemoryMasterApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MemoryMasterApplication.class, args);
 	}
-	@GetMapping
-	public String helloWorld() {
-		return "Hello World";
-	}
 
-	@Bean
-	public CommandLineRunner demo(UserRepository userRepository) {
-		return (args) -> {
-			System.out.println("=== Users in DB ===");
-			for (User u : userRepository.findAll()) {
-				System.out.println(u.getUser_id() + " | " + u.getUsername() + " | " + u.getEmail());
-			}
-		};
-	}
 }
