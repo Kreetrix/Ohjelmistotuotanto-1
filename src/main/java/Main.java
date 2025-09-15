cb7ca97778910f3b10a89240ed5314c863308ebb
George <gosha3484@gmail.com>
2025-09-15 16:44:40 +0300

    returned gui files

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+import java.sql.SQLException;
+import view.View;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        View.launch(View.class);
+    }
+
+}
