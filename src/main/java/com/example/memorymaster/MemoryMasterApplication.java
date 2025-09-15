aa4415a63e1aee4cca0897ea11393aeb79a926e2
George <gosha3484@gmail.com>
2025-09-15 14:57:12 +0300

    refactored app.properties

--- src/main/java/com/example/memorymaster/MemoryMasterApplication.java
+++ src/main/java/com/example/memorymaster/MemoryMasterApplication.java
@@ -1,4 +1,5 @@
 package com.example.memorymaster;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemoryMasterApplication {
 
 	public static void main(String[] args) {
-
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_DATABASE", dotenv.get("DB_DATABASE"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(MemoryMasterApplication.class, args);
 	}
 
