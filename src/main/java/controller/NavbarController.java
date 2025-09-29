ae07f9b74078b482c75aa55a399ed2cc22ed996d
skogbergP <SkogbergP@gmail.com>
2025-09-29 11:41:06 +0300

    feat: add logout button to navbar and integrate logout functionality

--- src/main/java/controller/NavbarController.java
+++ src/main/java/controller/NavbarController.java
@@ -4,7 +4,12 @@ import components.CustomButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+import java.io.IOException;
+
 public class NavbarController {
+    private LogoutController logout = new  LogoutController();
+    @FXML
+    public CustomButton logoutBtn;
 
     @FXML
     private Button searchBtn;
@@ -22,5 +27,13 @@ public class NavbarController {
         listBtn.setTooltipText("List");
         listBtn.setOnAction(e -> System.out.println("List clicked"));
         studyBtn.setOnAction(e -> System.out.println("Study clicked"));
+        logoutBtn.setIcon("/icons/logout.png");
+        logoutBtn.setOnAction(e -> {
+            try {
+                logout.onLogout();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
     }
 }
