5a838a21f4cdc65198dfebcd0a5ae7d8f44cdcba
George <gosha3484@gmail.com>
2025-10-03 20:21:59 +0300

    added a few comments and plugin JavaDoc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,14 @@ import model.entity.Cards;
 import model.entity.Decks;
 import view.View;
 
+/**
+ * The main entry point of the application.
+ * Initializes DAOs for users, cards, and decks, retrieves all records from the database,
+ * and prints them to the console. If the application is not running in headless mode,
+ * launches the JavaFX view.
+ */
+
+
 public class Main {
 
     public static void main(String[] args) throws SQLException {
