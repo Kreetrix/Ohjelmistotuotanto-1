1553e97bf52cca4017f8215fee5bd1db285cf916
Patrik Skogberg <skogbergp@gmail.com>
2025-11-27 13:49:56 +0200

    test: add PageLoaderTest for testing page loading functionality

---
+++ src/test/java/util/PageLoaderTest.java
@@ -0,0 +1,49 @@
+package util;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PageLoaderTest extends ApplicationTest {
+
+    static PageLoader pageLoader;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        pageLoader = PageLoader.getInstance();
+    }
+
+    @Test
+    void getInstance() {
+        assertSame(pageLoader,PageLoader.getInstance());
+    }
+
+    @Test
+    void loadPage() {
+       interact(() -> assertNotNull(pageLoader.loadPage("/fxml/loginView.fxml", "test")));
+    }
+
+    @Test
+    void loadPopUp() {
+        interact(() -> {
+            Stage popup = pageLoader.loadPopUp("/fxml/main.fxml", "test");
+            assertNotNull(popup);
+
+        });
+    }
+
+
+}
